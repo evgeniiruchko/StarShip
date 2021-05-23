62f9e638e2e1fcce15332f602d600f14ff04177e
EvgeniiRuchko <gk1987@mail.ru>
2021-05-23 19:21:10 +0300

    Initial commit

---
+++ desktop/src/ru/geekbrans/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.geekbrans.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.geekbrans.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
